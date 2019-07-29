67f8669c4003e9f206c0f5bfc3cbd1c4081e48a7
Aaron Altounian <aaron.altounian@gmail.com>
2019-07-28 20:14:23 -0500

    added webpack from client side

---
+++ public/precache-manifest.012eba31604fb910ba4f2159fb3b605b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bd41d40aa3340c8c34269397e0615c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1ecc2b2b6a693b8acf0",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "865c55006f7fc3c91ef8",
+    "url": "/static/css/main.c884d4fc.chunk.css"
+  },
+  {
+    "revision": "a1ecc2b2b6a693b8acf0",
+    "url": "/static/js/2.0db995c4.chunk.js"
+  },
+  {
+    "revision": "865c55006f7fc3c91ef8",
+    "url": "/static/js/main.1a756125.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2d50674d5045656234c3714143be2b92",
+    "url": "/static/media/airplane-logo.2d50674d.svg"
+  }
+]);
