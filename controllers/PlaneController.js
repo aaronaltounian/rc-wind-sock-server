93770c4d0470ce2a1fafd2cde76f218a1b50d92d
Aaron Altounian <aaron.altounian@gmail.com>
2019-08-02 17:09:22 -0500

    worked on Plane create function. Smoothed out search-location get request so it uses a default lat/lng if no zipcode is provided. Right now this data is coming from navigator.geolocation device geo data.

--- controllers/PlaneController.js
+++ controllers/PlaneController.js
@@ -3,6 +3,21 @@ const jwt = require('jsonwebtoken');
 
 const create = (req, res, next) => {
     console.log(req.body);
+    let token = req.headers.authorization;
+    console.log(token);
+    token = token.slice(7, token.length);
+    let user = jwt.verify(token, 'secretkey');
+    let id = user['userId'];
+    let plane = new Plane({
+        'userId': id,
+        'make': req.body.make,
+        'model': req.body.model,
+        'minWind': req.body.minWind,
+        'maxWind': req.body.maxWind,
+        'maxGust': req.body.maxGust
+    })
+    plane.save(() => res.json({'saved': true}))
+
 }
 
 const retrieve = (req, res, next) => {
--- routes/api/SearchLocations.js
+++ routes/api/SearchLocations.js
@@ -2,11 +2,10 @@ const fetch = require('node-fetch');
 const express = require('express');
 
 module.exports = (app) => {
-    let zipcode;
-
+    let zipcode = '';
+    let geolocation;
     app.post('/search-location', (req, res) => {
         zipcode = req.body.zipcode;
-
         if(!zipcode || zipcode.length < 5 || zipcode.length > 5) {
             res.redirect('/error');
         } else {
@@ -15,28 +14,42 @@ module.exports = (app) => {
     });
 
     app.get('/search-location-weather', (req, res) => {
-        const googleGeocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.REACT_APP_GOOGLE_API_KEY}&components=postal_code:${zipcode}`;
-
-        let lat, lng;
-        let response = {}
-        fetch(googleGeocodeUrl)
-        .then(res => res.json())
-        .then(data => {
-            response.geocode = data;
-            lat = data.results[0].geometry.location.lat;
-            lng = data.results[0].geometry.location.lng;
-        })
-        .then( data => {
-            fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${lat},${lng}`)
+        geolocation = {lat: Number(req.query.lat), lng: Number(req.query.lng)};
+        console.log(geolocation);
+        if(zipcode === '') {
+            fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${geolocation.lat},${geolocation.lng}`)
             .then(res => res.json())
             .then(data => {
-                response.weather = data;
-                console.log(response.geocode)
                 res.send(data);
             })
             .catch(err => {
                 res.redirect('/error');
-            });   
-        })
+            });               
+        }
+        else {
+            const googleGeocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.REACT_APP_GOOGLE_API_KEY}&components=postal_code:${zipcode}`;
+
+            let lat, lng;
+            let response = {}
+            fetch(googleGeocodeUrl)
+            .then(res => res.json())
+            .then(data => {
+                response.geocode = data;
+                lat = data.results[0].geometry.location.lat;
+                lng = data.results[0].geometry.location.lng;
+            })
+            .then( data => {
+                fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${lat},${lng}`)
+                .then(res => res.json())
+                .then(data => {
+                    response.weather = data;
+                    console.log(response.geocode)
+                    res.send(data);
+                })
+                .catch(err => {
+                    res.redirect('/error');
+                });   
+            })
+        }
     })
 }
